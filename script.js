ea36bec2c7d77d10940be0d249dada12f09ea319
IsaiahAngell <123341479+IsaiahAngell@users.noreply.github.com>
2023-10-10 19:38:39 -0300

    Trocar function por const

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ const loop = setInterval(() => {
         //ESSA CONDIÇÃO TESTARÁ SE A POKEBOLA ESTÁ A UMA DETERMINADA POSIÇÃO EM RELAÇÃO À ESQUERDA E SE O POKÉMON ESTÁ A UMA DETERMINADA POSIÇÃO EM RELAÇÃO À PARTE INFERIOR DA TELA
         //CASO A POKEBOLA ESTEJA A 510 PIXELS DA ESQUERDA E, SIMULTANEAMENTE, O PIKACHU ESTEJA A MENOS QUE 20 PIXELS DO CHÃO, O JOGO SERÁ INTERROMPIDO
         if ((pokeballPosition >= 510 && pikachuPosition < 20) || (ultraballPosition >= 510 && pikachuPosition < 20) || (masterballPosition >= 510 && pikachuPosition < 20)) {
-            //LINHAS DE CÓDIGO CRIADAS COM O OBJETIVO DE PARAR A ANIMAÇÃO DA POKEBOLA E FIXÁ-LA NA POSIÇÃO EM QUE TOCOU NO POKÉMON, VARIANDO CONFORME O TIPO DA POKEBOLA
+            //LINHAS DE CÓDIGO CRIADAS COM O OBJETIVO DE
+            PARAR A ANIMAÇÃO DA POKEBOLA E FIXÁ-LA NA POSIÇÃO EM QUE TOCOU NO POKÉMON, VARIANDO CONFORME O TIPO DA POKEBOLA
             pokeball.style.animation = 'none';
             pokeball.style.left = `${pokeballPosition}px`;
             ultraball.style.animation = 'none';
@@ -102,6 +103,6 @@ document.addEventListener('keydown', function(press){
 })
 
 //FUNÇAO PARA SAIR DA CAPA DO JOGO E IR PARA O JOGO
-function start(){ 
+const start(){ 
     window.location.href = "game.html";
 }
